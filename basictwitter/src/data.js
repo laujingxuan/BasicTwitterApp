fcc7e98bb4075b3d01655c81a89e9cb0aab65286
jingxuan94 <E0639666@u.nus.edu>
2021-01-09 12:57:12 +0800

    basic setup for REACT v1.0

---
+++ basictwitter/src/data.js
@@ -0,0 +1,8 @@
+export const data = [
+    { id: 1, user:'John', comment: 'First comment' },
+    { id: 2, user:'Mary', comment: 'Second comment' },
+    { id: 3, user:'John', comment: 'Third comment' },
+    { id: 4, user:'Mary', comment: 'Forth comment' },
+];
+
+//just some random data to use before connecting to BackEnd SpringBoot
